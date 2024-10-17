4ffe33346b592d7ee1773fc69406759f53264169
Nick-Almeida <nick.962@outlook.com>
2024-10-16 21:40:00 -0300

    Continuação e exercícios Aula 02 CSS

---
+++ JS/JS02-Console/JS02-ExercicioCasa/main.js
@@ -0,0 +1,9 @@
+let nota1, nota2, nota3, nota4, media
+
+nota1 = 6
+nota2 = 2
+nota3 = 8
+nota4 = 4
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+console.log("A nota do aluno com 4 notas é: " + media)
--- JS/JS02-Console/main.js
+++ JS/JS02-Console/main.js
@@ -1,9 +1,57 @@
+'use strict'
 //Quando eu quero criar uma variável, preciso inserir antes do nome dessa variável as palavras reservadas var/let/const
 
-var nomeAluno = 0
+// let nomeAluno = 0
 
-console.log(nomeAluno)
+// console.log(nomeAluno)
 
-nomeAluno = "nome"
+// nomeAluno = "nome"
 
-console.log(nomeAluno)
+// console.log(nomeAluno)
+
+// let valor, num1, num2
+// num1 = 10
+// num2 = 20
+
+// valor = num1 > num2
+// valor = num1 >= num2
+// valor = num1 < num2
+// valor = num1 <= num2
+// valor = num1 == num2
+// valor = num1 === num2
+// valor = num1 != num2
+// console.log(valor)
+
+// let nome = "Ermeson"
+// let num = 5
+// let num1 = 10
+// console.log(typeof num)
+// console.log(num + num1)
+// console.log(nome.length)
+
+// let nota1, nota2, nota3, nota4, media, faltas
+
+// nota1 = 7
+// nota2 = 4
+// nota3 = 8
+// nota4 = 8
+// faltas = 5
+// media = (nota1 + nota2 + nota3 + nota4) / 4
+
+// let res = media >= 7 && faltas <= 5 ? "Aprovado" : "Reprovado"
+
+// console.log("Sua média foi: " + media + ".","Suas faltas foram: " + faltas + ".", "Você foi " + res);
+ 
+// let nome = prompt("Qual o seu nome?")
+// alert(`Olá ${nome}!`)
+
+alert("Olá, Mundo!")
+
+let nome = prompt("Qual seu nome?")
+alert ("Bem vindo " + nome)
+
+let idade = prompt("Qual sua idade?")
+
+let maiorIdade = idade >= "18"
+? "maior de idade" : "menor de idade"
+alert ("Você é " + maiorIdade)
