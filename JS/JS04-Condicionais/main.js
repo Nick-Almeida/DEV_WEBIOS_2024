085991a4f742f929c9b951b4a6fda0f1b721f714
Nick-Almeida <nick.962@outlook.com>
2024-10-21 21:42:21 -0300

    Começo Aula 04 JS

---
+++ JS/JS04-Condicionais/main.js
@@ -0,0 +1,33 @@
+//Verificar se o Caio está com a blusa do IOS
+
+let CaioComBlusa = true
+
+if (CaioComBlusa == true) {
+    console.log("Caio pode assistir a aula!");
+}else{
+    console.log("Caio não pode ver a aula");
+}
+
+console.log("//////////////////////////////////////////////////////////////////")
+
+// Um jogador somente irá passar para a fase 2 se atingir 1000 pontos
+
+let pontos = 1000
+
+if (pontos >= 1000) {
+    console.log("Você passou para a fase 2!");
+}else{
+    console.log("Foi por pouco, tente novamente!");
+}
+
+console.log("//////////////////////////////////////////////////////////////////")
+
+// Teste se número é par ou impar
+
+let imparPar= 18
+
+if (imparPar % 2 == 0) {
+    console.log("Este número é par");
+}else{
+    console.log("Este número é ímpar");
+}
