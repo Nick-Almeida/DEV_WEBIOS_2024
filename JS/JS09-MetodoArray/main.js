199ce07584f6079940a472634b7d89011fcd450e
Nick-Almeida <nick.962@outlook.com>
2024-11-07 21:31:30 -0300

    Aula JS09

---
+++ JS/JS09-MetodoArray/main.js
@@ -0,0 +1,21 @@
+// Array
+
+let frutas = ["Amora", "Banana", "Abacate", "Jabuticaba", "Abacaxi", "Manga", "Kiwi", "Uva"] // Coloque o nome de 8 frutas
+
+frutas.forEach((valor, indice) =>{
+    console.log(valor); // Conteúdo da posição
+    console.log(indice); // Número da posição
+})
+
+// frutas.forEach(Percorrer)
+
+//               conteúdo - indice
+// function Percorrer(valor, indice){
+//   console.log(valor); // Conteúdo da posição
+//   console.log(indice); // Número da posição
+// }
+
+// for(i = 0; frutas.length > i; i++){
+//   console.log(frutas[i]); // Conteúdo da posição
+//   console.log(i); // Número da posição
+// }
