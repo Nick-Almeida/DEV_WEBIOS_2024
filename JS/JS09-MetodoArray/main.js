53607c86046c08e986a51e45437101472266153d
Nick-Almeida <nick.962@outlook.com>
2024-11-08 20:51:42 -0300

    Continuação aula JS09

--- JS/JS09-MetodoArray/main.js
+++ JS/JS09-MetodoArray/main.js
@@ -19,3 +19,52 @@ frutas.forEach((valor, indice) =>{
 //   console.log(frutas[i]); // Conteúdo da posição
 //   console.log(i); // Número da posição
 // }
+
+console.clear()
+
+// Array de marcas de celular com 6 posições
+
+// Math.currently / Math.max / Math.min / Math.sqrt
+let numeros = [16, 22, 59, 94, 36, 71]
+
+// Trazer um novo array atualizado com a raiz quadrada de cada número do array
+let numerosDois = numeros.map(Math.sqrt)
+let maiorNúmero = Math.min.apply(Math, numeros)
+
+console.log(numeros);
+console.log(numerosDois);
+console.log(maiorNúmero);
+
+console.clear()
+
+// Criando um novo array que ira conter apenas os números maiores que 36
+let filtraNumero = numeros.filter((valor) =>{
+    return valor >= 36
+})
+
+console.log(filtraNumero);
+
+console.clear()
+
+let tarefa = [
+    {
+        tarefa: "Dormir",
+        isFeita: true
+    },
+    {
+        tarefa: "Limpar",
+        isFeita: false
+    },
+    {
+        tarefa: "Torcer",
+        isFeita: true
+    }
+]
+
+// retorne apenas as tarefas que estão marcadas como true
+// find
+let retornaTrue = tarefa.find((item) => {
+    return item.isFeita == true
+})
+
+console.log(retornaTrue);
