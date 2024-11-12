82e7fbf1ef48673ff5e49d7600ed85fa743e2b17
Nick-Almeida <nick.962@outlook.com>
2024-11-12 19:37:50 -0300

    Continuação aula JS10

--- JS/JS10-Classes/main.js
+++ JS/JS10-Classes/main.js
@@ -9,9 +9,13 @@ class Aluno  {
     }
 
     retornaIdade(){
-        
+        let dataCompleta = new Date();
+        let pegaAno = dataCompleta.getFullYear()
+        return pegaAno - this.ano
     }
 }
+
+// Classe no JS: Date -> Retorna informações de data de acordo com o que está configurado no seu computador/servidor
 
 //instancia == cópias
 let aluno1 = new Aluno ("Manasses", 1997, 1.90, true, true, "Black")
@@ -19,4 +23,6 @@ let aluno1 = new Aluno ("Manasses", 1997, 1.90, true, true, "Black")
 let aluno2 = new Aluno ("Jeferson", 2005, 1.75, true, true, "White")
 
 console.log(aluno1)
+console.log(aluno1.retornaIdade());
 console.log(aluno2)
+console.log(aluno2.retornaIdade());
